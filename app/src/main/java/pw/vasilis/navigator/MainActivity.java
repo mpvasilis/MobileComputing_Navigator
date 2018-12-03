ffbcd9a78b5befe157691536bbca1d1456dbd32c
Vasileios Balafas <me@vasilis.pw>
2018-12-03 22:49:01 +0200

    Added day/night theme

--- app/src/main/java/pw/vasilis/navigator/MainActivity.java
+++ app/src/main/java/pw/vasilis/navigator/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationManager;
+import android.support.constraint.ConstraintLayout;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -18,6 +19,9 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.math.BigDecimal;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Formatter;
 import java.util.Locale;
 
@@ -28,12 +32,17 @@ public class MainActivity extends AppCompatActivity  implements GPSCallback{
     LocationManager locationManager;
     double currentSpeed,kmphSpeed;
     TextView txtview;
+    ImageButton imageButton;
+    ConstraintLayout constraintLayout;
+    String check_day_night = "day";
+    double lat;
+    double longt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        constraintLayout= findViewById(R.id.clayout);
         txtview = findViewById(R.id.speed);
         try {
             if (ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ) {
@@ -52,15 +61,23 @@ public class MainActivity extends AppCompatActivity  implements GPSCallback{
             gpsManager.showSettingsAlert();
         }
 
-        final ImageButton imageButton = findViewById(R.id.imageButton);
+        imageButton = findViewById(R.id.imageButton);
 
         imageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(MainActivity.this, MapsActivity.class);
+                i.putExtra("lat",lat);
+                i.putExtra("long",longt);
                 startActivity(i);
             }
         });
+
+        try {
+            checkDayNight();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
 
     }
 
@@ -68,8 +85,8 @@ public class MainActivity extends AppCompatActivity  implements GPSCallback{
     @Override
     public void onGPSUpdate(Location location) {
         speed = location.getSpeed();
-        double lat = location.getLatitude();
-        double longt = location.getLongitude();
+        lat = location.getLatitude();
+        longt = location.getLongitude();
         currentSpeed = round(speed,3,BigDecimal.ROUND_HALF_UP);
         kmphSpeed = round((currentSpeed*3.6),0,BigDecimal.ROUND_HALF_UP);
         int kmph_int= (int) kmphSpeed;
@@ -88,6 +105,30 @@ public class MainActivity extends AppCompatActivity  implements GPSCallback{
         BigDecimal bd = new BigDecimal(unrounded);
         BigDecimal rounded = bd.setScale(precision, roundingMode);
         return rounded.doubleValue();
+    }
+
+    public void checkDayNight() throws ParseException {
+        SimpleDateFormat formatter = new SimpleDateFormat("kk:mm");
+        Date date_from = formatter.parse("06:00");
+        Date date_to = formatter.parse("18:00");
+        Date dateNow = formatter.parse(formatter.format(new Date()));
+
+        System.out.println("date_to: " + date_from);
+        System.out.println("date_to: " + date_to);
+        System.out.println("dateNow: " + dateNow);
+
+
+            if (dateNow.after(date_from) && dateNow.before(date_to)) {
+                check_day_night = "day";
+                constraintLayout.setBackgroundResource(R.drawable.wbg);
+                imageButton.setImageResource(R.drawable.wmap);
+            } else {
+                check_day_night = "night";
+                constraintLayout.setBackgroundResource(R.drawable.bbg);
+                imageButton.setImageResource(R.drawable.bmap);
+            }
+
+        System.out.println("Now is " + check_day_night);
     }
 
 
--- app/src/main/java/pw/vasilis/navigator/MapsActivity.java
+++ app/src/main/java/pw/vasilis/navigator/MapsActivity.java
@@ -1,5 +1,6 @@
 package pw.vasilis.navigator;
 
+import android.app.ActionBar;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
@@ -13,6 +14,8 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    double lat=0;
+    double longt=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,25 +25,20 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        lat = getIntent().getDoubleExtra("lat", 0);
+        longt = getIntent().getDoubleExtra("long", 0);
+
+
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        LatLng sydney = new LatLng(lat, longt);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("YOU"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
